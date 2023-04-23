f94d742330266f958d4357d3d4ef089291bf227b
yodhcn <47470844+yodhcn@users.noreply.github.com>
2023-04-23 15:28:07 +0800

    8.6. Styling the Product Items

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,14 +1,18 @@
 import React from "react";
-import { FlatList, Text } from "react-native";
+import { FlatList } from "react-native";
 
 import { useBoundStore } from "../../stores/useBoundStore";
+import ProductItem from "../../components/shop/ProductItem";
 
 export default function ProductsOverviewScreen(props) {
   const products = useBoundStore((state) => state.availableProducts);
   return (
     <FlatList
       data={products}
-      renderItem={(itemData) => <Text>{itemData.item.title}</Text>}
+      renderItem={(itemData) => {
+        const { imageUrl, title, price } = itemData.item;
+        return <ProductItem imageUrl={imageUrl} title={title} price={price} />;
+      }}
     />
   );
 }
