05cd7ea14dbca15c6d20c9383f19fb24070c13b4
yodhcn <47470844+yodhcn@users.noreply.github.com>
2023-04-23 20:20:50 +0800

    8.8. Working on the Product Details Screen

--- screens/shop/ProductDetailScreen.js
+++ screens/shop/ProductDetailScreen.js
@@ -9,14 +9,44 @@ import {
 } from "react-native";
 
 import { useBoundStore } from "../../stores/useBoundStore";
+import Colors from "../../constants/Colors";
 
-export default function ProductDetailScreen({ route }) {
-  const selectedProduct = useBoundStore((state) =>
-    state.availableProducts.find((prod) => prod.id === route.params.prodId)
+export default function ProductDetailScreen(props) {
+  const prodId = props.route.params.prodId;
+  const product = useBoundStore((state) =>
+    state.availableProducts.find((prod) => prod.id === prodId)
   );
+
   return (
-    <View>
-      <Text>{selectedProduct.title}</Text>
-    </View>
+    <ScrollView>
+      <Image style={styles.image} source={{ uri: product.imageUrl }} />
+      <View style={styles.actions}>
+        <Button color={Colors.primary} title="Add To Cart" />
+      </View>
+      <Text style={styles.price}>${product.price.toFixed(2)}</Text>
+      <Text style={styles.description}>{product.description}</Text>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+    height: 300,
+  },
+  actions: {
+    marginVertical: 10,
+    alignItems: "center",
+  },
+  price: {
+    fontSize: 20,
+    color: "#888",
+    textAlign: "center",
+    marginVertical: 20,
+  },
+  description: {
+    fontSize: 14,
+    textAlign: "center",
+    marginHorizontal: 20,
+  },
+});
