896744b332cb1aed812a2858faed9fe8484bf251
yodhcn <47470844+yodhcn@users.noreply.github.com>
2023-04-24 21:25:12 +0800

    Update useBoundStore.js

--- stores/useBoundStore.js
+++ stores/useBoundStore.js
@@ -3,10 +3,12 @@ import { immer } from "zustand/middleware/immer";
 
 import { createProductsSlice } from "./productsSlice";
 import { createCartSlice } from "./cartSlice";
+import { createOrdersSlice } from "./ordersSlice";
 
 export const useBoundStore = create(
   immer((...a) => ({
     ...createProductsSlice(...a),
     ...createCartSlice(...a),
+    ...createOrdersSlice(...a),
   }))
 );
