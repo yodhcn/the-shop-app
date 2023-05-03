370ecd15554094c07d2f594d9c657248661ebdc3
yodhcn <47470844+yodhcn@users.noreply.github.com>
2023-05-03 19:38:07 +0800

    10.10. Storing Orders

--- stores/ordersSlice.js
+++ stores/ordersSlice.js
@@ -1,15 +1,28 @@
+import axios from "axios";
+
 import Order from "../models/order";
 import { createCartSlice } from "./cartSlice";
 
 export const createOrdersSlice = (set) => ({
   orders: [],
-  addOrder: (cartItems, cartTotalAmount) => {
-    set((state) => {
-      const newOrder = new Order(
-        new Date().toString(),
+  addOrder: async (cartItems, cartTotalAmount) => {
+    const date = new Date();
+    const response = await axios.post(
+      "https://rn-complete-guide-66dfd-default-rtdb.asia-southeast1.firebasedatabase.app/orders/u1.json",
+      {
         cartItems,
         cartTotalAmount,
-        new Date()
+        date: date.toISOString(),
+      }
+    );
+    const resData = response.data;
+
+    set((state) => {
+      const newOrder = new Order(
+        resData.name,
+        cartItems,
+        cartTotalAmount,
+        date
       );
       state.orders.push(newOrder);
     });
